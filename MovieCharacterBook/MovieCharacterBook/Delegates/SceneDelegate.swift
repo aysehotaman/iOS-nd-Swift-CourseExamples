01ec656552fd2a809c1826ec7fb2b0e5f402dbd0
ayse <aysehotaman2@gmail.com>
2022-11-10 21:39:40 +0300

    Solve Project Target Problem

--- MovieCharacterBook/MovieCharacterBook/Delegates/SceneDelegate.swift
+++ MovieCharacterBook/MovieCharacterBook/Delegates/SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  SceneDelegate.swift
-//  GreensBook
+//  MovieCharacterBook
 //
-//  Created by Ayşe Hotaman on 8.11.2022.
+//  Created by Ayşe Hotaman on 10.11.2022.
 //
 
 import UIKit
@@ -45,6 +45,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
